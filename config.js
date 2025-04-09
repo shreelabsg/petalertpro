d019539a0630f421b5119f9da0444f4f33dc5cd6
shreelabsg <contact.shreelabs@gmail.com>
2025-04-09 09:31:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Detect if the website is running on staging or production
-const isStaging = true;
-const PADDLE_MODE = isStaging ? "sandbox" : "sandbox"; //production or sandbox
+const isStaging = false;
+const PADDLE_MODE = isStaging ? "sandbox" : "production"; //production or sandbox
 
 const SUPPORT_EMAIL = "support@petalertpro.com";
 // Define Cloudflare API base URLs
@@ -9,10 +9,10 @@ const API_BASE_URL = isStaging
   : "https://petalertpro.contact-shreelabs.workers.dev";
 
 // Paddle Configuration (No direct Paddle reference here)
-const PADDLE_SANDBOX_PRICE_ID =  "pri_01jpm9zbzcst31xfzanqk4y6ms";  // ✅ Sandbox Price ID
+const PADDLE_SANDBOX_PRICE_ID =  "";  // ✅ Sandbox Price ID
 const PADDLE_LIVE_PRICE_ID = "pri_01jpm9ze2ry6f9rfk7gs7grcaq"; // ✅ Live Price ID
 
-const PADDLE_SANDBOX_CLIENT_TOKEN =  "test_cbc599ae07d129ebca777da129d"; // 
+const PADDLE_SANDBOX_CLIENT_TOKEN =  ""; // 
 const PADDLE_LIVE_CLIENT_TOKEN = "live_85cadfbd55315b2bf3f4bc58653"; // 
 
 const CLIENT_TOKEN = isStaging || PADDLE_MODE === "sandbox" ? PADDLE_SANDBOX_CLIENT_TOKEN : PADDLE_LIVE_CLIENT_TOKEN;
